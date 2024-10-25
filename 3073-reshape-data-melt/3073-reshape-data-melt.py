267ada21701215a0c674d847dc9086976967e053
Lata Sharma <96385877+Latasharma26@users.noreply.github.com>
2024-10-25 10:39:25 +0530

    Time: 444 ms (30.75%), Space: 69.2 MB (71.65%) - LeetHub

---
+++ 3073-reshape-data-melt/3073-reshape-data-melt.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    result = report.melt(id_vars=['product'], 
+                         var_name='quarter', 
+                         value_name='sales')
+    return result
